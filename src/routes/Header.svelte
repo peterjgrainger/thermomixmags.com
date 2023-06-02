1877110061b853820604a70d8dc64e6ffb35337f
Peter Grainger <peter@grainger.xyz>
2023-06-02 20:12:21 +0100

    add some more data for the page

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -7,7 +7,8 @@
     <div class = "header-container">
         <div>
           <h1> thermomix mags </h1>
-          <h3> Independent Thermomix Consultant </h3>
+          <h3> Independent Thermomix Advisor </h3>
+          <p> Gosforth, Newcastle-upon-tyne</p>
         </div>
     </div>
 </header>
@@ -31,20 +32,25 @@
     h1 {
       font-size: 6rem;
     }
+    h3 {
+      font-size: 3rem;
+    }
+
+    p {
+      font-size: 2.5rem;
+      display: block;
+    }
   }
   @media(max-width: 850px) {
     h1 {
       font-size: 3rem;
       display: block;
     }
-  }
 
-  @media(min-width: 750px) {
-    h3 {
-      font-size: 2rem;
+    p {
+      font-size: 1rem;
+      display: block;
     }
-  }
-  @media(max-width: 750px) {
     h3 {
       font-size: 1rem;
       display: block;
@@ -65,12 +71,20 @@
   h3 {
     margin-top: 0rem;
     text-align: center;
+    margin-bottom: 0rem;
     
   }
 
   h1 {
     margin-bottom: 0rem;
+    margin-top: 0rem;
     text-align: center;
     color: var(--green)
+  }
+
+  p {
+    margin-top: 0rem;
+    margin-bottom: 0rem;
+    text-align: center;
   }
 </style>
